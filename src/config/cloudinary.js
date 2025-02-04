579e0282abe6cacbcd1c0dde2a07641acd554cfd
Geo manu k <148890291+geomanuk20@users.noreply.github.com>
2025-02-04 15:11:54 +0530

    Add files via upload

---
+++ src/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dja3cpn2i',
+  api_key: '368338978515832',
+  api_secret: 'K6CE-c2h1IYyrpwx3rEB38QpJJc',
+});
+
+module.exports = cloudinary;
